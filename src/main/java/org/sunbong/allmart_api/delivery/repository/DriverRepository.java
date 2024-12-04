787ab3c5bd7bfff8e95ae2bfa35ab31d4445e2e7
hajaemin <98006326+ha-jae-min@users.noreply.github.com>
2024-12-04 13:55:20 +0900

    Revert "feat: kafka outbox패턴 구현 - 주문 배달 (#119)"
    This reverts commit cd13a1b0cfe3298c165db0916a7d3460446a8bcd.

--- src/main/java/org/sunbong/allmart_api/delivery/repository/DriverRepository.java
+++
@@ -1,8 +0,0 @@
-package org.sunbong.allmart_api.delivery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.sunbong.allmart_api.delivery.domain.DriverEntity;
-
-public interface DriverRepository extends JpaRepository<DriverEntity, Long> {
-    DriverEntity findFirstByMaxDeliveryCountGreaterThanOrderByIdAsc(int minDeliveryCount);
-}
--- src/main/java/org/sunbong/allmart_api/order/dto/OrderEvent.java
+++
@@ -1,9 +0,0 @@
-package org.sunbong.allmart_api.order.dto;
-
-import lombok.Data;
-
-@Data
-public class OrderEvent {
-    private Long orderId;
-    private String customerId;
-}
